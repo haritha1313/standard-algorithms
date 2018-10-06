4e5092b31cdced1c141746c44161b4139a647f42
Nattapat <Tatsuya10199@gmail.com>
2018-10-07 03:50:19 +0700

    Align code

--- dynamic_programming/coin-change.cpp
+++ dynamic_programming/coin-change.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+
 	int t,n,c;
 	cin >> t;
 	while(t--)
 	{
 	    cin >> c;
 	    int coin[c];
-	    for(int i=0;i<c;i++)
+	    
+        for(int i=0;i<c;i++) {
             cin >> coin[i];
+        }
+
         cin >> n;
         int memory[n+1];
-        for(int i=0;i<=n;i++)
+        
+        for(int i=0;i<=n;i++) {
             memory[i] = 0;
+        }
+
         memory[0] = 1;
 
         for(int j=0;j<c;j++) {
@@ -21,7 +27,9 @@ int main() {
                 memory[i] += memory[i-coin[j]];
             }
         }
-	    cout << memory[n] << endl;
+	    
+        cout << memory[n] << endl;
 	}
 	return 0;
+    
 }
